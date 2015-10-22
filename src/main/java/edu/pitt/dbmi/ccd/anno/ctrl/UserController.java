48e9306da0e64fa5b782cd2ce083f5864e4f077c
Mark Silvis <masilvis22@gmail.com>
2015-10-22 18:31:38 -0400

    added controllers for five main categories of data

---
+++ src/main/java/edu/pitt/dbmi/ccd/anno/ctrl/UserController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package edu.pitt.dbmi.ccd.anno.ctrl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import edu.pitt.dbmi.ccd.db.entity.UserAccount;
+import edu.pitt.dbmi.ccd.db.entity.Person;
+import edu.pitt.dbmi.ccd.db.service.UserAccountService;
+import edu.pitt.dbmi.ccd.db.service.PersonService;
+
+/**
+ * @author Mark Silvis (marksilvis@pitt.edu)
+ */
+@RestController
+@RequestMapping(value="users")
+public class UserController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    private final UserAccountService accountService;
+    private final PersonService personService;
+
+    @Autowired(required=true)
+    public UserController(UserAccountService accountService,
+                          PersonService personService) {
+        this.accountService = accountService;
+        this.personService = personService;
+    }
+}
