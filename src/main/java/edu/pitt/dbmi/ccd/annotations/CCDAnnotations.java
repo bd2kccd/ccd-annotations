92df4e72ca37a1ab069223edc783353647f20b15
Mark Silvis <marksilvis@pitt.edu>
2018-04-23 13:29:00 -0400

    working on causal-web 2.0 compliance

---
+++ src/main/java/edu/pitt/dbmi/ccd/annotations/CCDAnnotations.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.ccd.annotations;
+
+import edu.pitt.dbmi.ccd.db.CcdDbApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.hateoas.config.EnableEntityLinks;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+
+/**
+ * @author Mark Silvis
+ */
+@SpringBootApplication
+@Import({CcdDbApplication.class})
+@EnableEntityLinks
+@EnableHypermediaSupport(type = HypermediaType.HAL)
+public class CCDAnnotations {
+
+    public static void main(String[] args) {
+        ApplicationContext app = SpringApplication.run(CCDAnnotations.class, args);
+    }
+}
